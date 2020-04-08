d2b2dcfb1edfdac5ad2095f4cb0ef40faa653983
Santiago Torrabadella <santi_torri97@hotmail.com>
2020-04-08 12:28:49 +0200

    directory re-structure, services files & components files

--- practica1angular/src/app/app.module.ts
+++ practica1angular/src/app/app.module.ts
@@ -1,11 +1,15 @@
-/*import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { CrudComponent } from './modules/crud/components/crud/crud.component';
+import { LoginComponent } from './modules/login/components/login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -14,4 +18,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }*/
+export class AppModule { }
