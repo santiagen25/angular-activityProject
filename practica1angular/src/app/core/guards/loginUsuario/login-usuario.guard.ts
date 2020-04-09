1e2e63441f7df507eab8e9eae72d9938ba7b12c5
Santiago Torrabadella <santi_torri97@hotmail.com>
2020-04-09 13:55:16 +0200

    preparado loginUsuario guard

--- practica1angular/src/app/core/guards/loginUsuario/login-usuario.guard.ts
+++ practica1angular/src/app/core/guards/loginUsuario/login-usuario.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+//import { AuthService } from 'ruta.servicioLogin'; //importamos el servicio que usamos para loguear al usuario
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginUsuarioGuard implements CanActivate {
 
-  constructor(public router: Router) { }
+  constructor(/*public authService[operar con este nombre de variable, y mirar si está logueado en canActivate]: AuthService[poner aqui el servicio del login e importarlo],*/ public router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
