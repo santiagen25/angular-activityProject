f863fd750bdb9061b7880b47601a348e26f0e20b
Santiago Torrabadella <santi_torri97@hotmail.com>
2020-04-10 13:28:38 +0200

    login route implementada

--- practica1angular/src/app/app-routing.module.ts
+++ practica1angular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/login/components/login/login.component';
+import { LoginUsuarioGuard } from './core/guards/loginUsuario/login-usuario.guard';
+import { CrudComponent } from './modules/crud/components/crud/crud.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, canActivate: [LoginUsuarioGuard] },
+  { path: 'crud', component: CrudComponent, canActivate: [LoginUsuarioGuard] },
+  { path: '**', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
