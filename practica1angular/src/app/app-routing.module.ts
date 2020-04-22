97e8348befe071cba8ca2706776c3610106dca0b
Santiago Torrabadella <santi_torri97@hotmail.com>
2020-04-23 00:54:12 +0200

    routes & routing working properly

--- practica1angular/src/app/app-routing.module.ts
+++ practica1angular/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/login/components/login/login.component';
 import { LoginUsuarioGuard } from './core/guards/loginUsuario/login-usuario.guard';
 import { CrudComponent } from './modules/crud/components/crud/crud.component';
+import { NotFoundComponent } from './modules/crud/components/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [LoginUsuarioGuard] },
   { path: 'crud', component: CrudComponent, canActivate: [LoginUsuarioGuard] },
-  { path: '**', redirectTo: 'list', pathMatch: 'full' }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- practica1angular/src/app/app.module.ts
+++ practica1angular/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
+import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { CrudComponent } from './modules/crud/components/crud/crud.component';
 import { LoginComponent } from './modules/login/components/login/login.component';
+import { NotFoundComponent } from './modules/crud/components/not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     CrudComponent,
-    LoginComponent
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
